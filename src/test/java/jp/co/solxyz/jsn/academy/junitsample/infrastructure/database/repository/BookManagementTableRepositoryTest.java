21c242032eb52433542272de3805d7aecb5b18f0
hito <hiroto.ito@solxyz.co.jp>
2025-06-12 13:52:59 +0900

    ExtendWithの削除

--- src/test/java/jp/co/solxyz/jsn/academy/junitsample/infrastructure/database/repository/BookManagementTableRepositoryTest.java
+++ src/test/java/jp/co/solxyz/jsn/academy/junitsample/infrastructure/database/repository/BookManagementTableRepositoryTest.java
@@ -4,12 +4,10 @@ import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import jp.co.solxyz.jsn.academy.junitsample.infrastructure.database.dto.BookManagementTableDto;
 
@@ -21,7 +19,6 @@ import jp.co.solxyz.jsn.academy.junitsample.infrastructure.database.dto.BookMana
  * @author JSN
  */
 @DataJpaTest
-@ExtendWith(SpringExtension.class)
 class BookManagementTableRepositoryTest {
 
   @Autowired
