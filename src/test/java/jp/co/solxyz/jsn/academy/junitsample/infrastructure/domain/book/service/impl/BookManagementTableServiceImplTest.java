c5ad7352ad55163141004dfe2ff772b1438997cd
hito <hiroto.ito@solxyz.co.jp>
2025-06-13 16:35:01 +0900

    TableServiceTestもメソッド名を英語に修正

--- src/test/java/jp/co/solxyz/jsn/academy/junitsample/infrastructure/domain/book/service/impl/BookManagementTableServiceImplTest.java
+++ src/test/java/jp/co/solxyz/jsn/academy/junitsample/infrastructure/domain/book/service/impl/BookManagementTableServiceImplTest.java
@@ -7,6 +7,7 @@ import static org.mockito.Mockito.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -38,7 +39,8 @@ class BookManagementTableServiceImplTest {
 	class SearchStockInfo {
 
 		@Test
-		void 書籍在庫情報リスト取得成功_2件() {
+		@DisplayName("書籍在庫情報リスト取得成功_2件")
+		void searchStockInfoSuccess2Records() {
 			List<BookManagementTableDto> mockList = new ArrayList<>();
 			mockList.add(new BookManagementTableDto());
 			mockList.add(new BookManagementTableDto());
@@ -50,7 +52,8 @@ class BookManagementTableServiceImplTest {
 		}
 
 		@Test
-		void 書籍在庫情報リスト取得成功_1件() {
+		@DisplayName("書籍在庫情報リスト取得成功_1件")
+		void searchStockInfoSuccess1Record() {
 			List<BookManagementTableDto> mockList = new ArrayList<>();
 			mockList.add(new BookManagementTableDto());
 
@@ -61,7 +64,8 @@ class BookManagementTableServiceImplTest {
 		}
 
 		@Test
-		void 書籍在庫情報リスト取得成功_0件() {
+		@DisplayName("書籍在庫情報リスト取得成功_0件")
+		void searchStockInfoSuccess0Records() {
 			List<BookManagementTableDto> emptyList = new ArrayList<>();
 
 			doReturn(emptyList).when(bookManagementTableRepository).findAll();
@@ -71,7 +75,8 @@ class BookManagementTableServiceImplTest {
 		}
 
 		@Test
-		void 書籍在庫情報リスト取得失敗() {
+		@DisplayName("書籍在庫情報リスト取得失敗")
+		void searchStockInfoFailure() {
 			doThrow(new DataIntegrityViolationException("DB Error")).when(bookManagementTableRepository).findAll();
 
 			assertThrows(DataIntegrityViolationException.class, () -> {
@@ -84,7 +89,8 @@ class BookManagementTableServiceImplTest {
 	class UpdateStockInfo {
 
 		@Test
-		void 書籍在庫情報更新成功() {
+		@DisplayName("書籍在庫情報更新成功")
+		void updateStockInfoSuccess() {
 			BookManagementTableDto dto = new BookManagementTableDto();
 
 			doReturn(dto).when(bookManagementTableRepository).saveAndFlush(dto);
@@ -94,7 +100,8 @@ class BookManagementTableServiceImplTest {
 		}
 
 		@Test
-		void 書籍在庫情報更新失敗() {
+		@DisplayName("書籍在庫情報更新失敗")
+		void updateStockInfoFailure() {
 			BookManagementTableDto dto = new BookManagementTableDto();
 
 			doThrow(new DataIntegrityViolationException("DB Error")).when(bookManagementTableRepository).saveAndFlush(dto);
