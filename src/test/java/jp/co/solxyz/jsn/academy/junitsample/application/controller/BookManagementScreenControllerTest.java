f3165ac27825374967622ddcfe4767aa50ae832a
hito <hiroto.ito@solxyz.co.jp>
2025-06-16 10:43:33 +0900

    メソッド名変更、DisplayName追加

--- src/test/java/jp/co/solxyz/jsn/academy/junitsample/application/controller/BookManagementScreenControllerTest.java
+++ src/test/java/jp/co/solxyz/jsn/academy/junitsample/application/controller/BookManagementScreenControllerTest.java
@@ -6,6 +6,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import jp.co.solxyz.jsn.academy.junitsample.infrastructure.database.dto.BookManagementTableDto;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,10 +29,12 @@ class BookManagementScreenControllerTest {
 	private BookManagementService bookManagementService;
 
 	@Nested
+	@DisplayName("初期化処理")
 	class Init {
 
 		@Test
-		void 書籍在庫情報リスト取得成功_0件() throws Exception {
+		@DisplayName("書籍在庫情報リスト取得成功_0件")
+		void initSuccess0Records() throws Exception {
 			// Arrange
 			List<BookManagementTableDto> emptyList = new ArrayList<>();
 			when(bookManagementService.init()).thenReturn(emptyList);
@@ -44,16 +47,14 @@ class BookManagementScreenControllerTest {
 		}
 
 		@Test
-		void 書籍在庫情報リスト取得成功_1件() throws Exception {
-			// Arrange
+		@DisplayName("書籍在庫情報リスト取得成功_1件")
+		void initSuccess1Record() throws Exception {
 			List<BookManagementTableDto> bookList = new ArrayList<>();
-
 			BookManagementTableDto book = new BookManagementTableDto();
 			bookList.add(book);
 
 			when(bookManagementService.init()).thenReturn(bookList);
 
-			// Act & Assert
 			mockMvc.perform(get("/manage/book"))
 					.andExpect(status().isOk())
 					.andExpect(view().name("bookmanager"))
@@ -61,18 +62,16 @@ class BookManagementScreenControllerTest {
 		}
 
 		@Test
-		void 書籍在庫情報リスト取得成功_2件() throws Exception {
-			// Arrange
+		@DisplayName("書籍在庫情報リスト取得成功_2件")
+		void initSuccess2Records() throws Exception {
 			List<BookManagementTableDto> bookList = new ArrayList<>();
-
 			BookManagementTableDto book1 = new BookManagementTableDto();
 			BookManagementTableDto book2 = new BookManagementTableDto();
-			bookList.add(book2);
 			bookList.add(book1);
+			bookList.add(book2);
 
 			when(bookManagementService.init()).thenReturn(bookList);
 
-			// Act & Assert
 			mockMvc.perform(get("/manage/book"))
 					.andExpect(status().isOk())
 					.andExpect(view().name("bookmanager"))
@@ -80,12 +79,11 @@ class BookManagementScreenControllerTest {
 		}
 
 		@Test
-		void 書籍在庫情報リスト取得失敗() throws Exception {
-			// Arrange
+		@DisplayName("書籍在庫情報リスト取得失敗")
+		void initFailure() throws Exception {
 			final String EXPECTED_MSG = "書籍情報がありません。";
 			when(bookManagementService.init()).thenReturn(null);
 
-			// Act & Assert
 			mockMvc.perform(get("/manage/book"))
 					.andExpect(status().isOk())
 					.andExpect(view().name("bookmanager"))
@@ -94,15 +92,15 @@ class BookManagementScreenControllerTest {
 	}
 
 	@Nested
+	@DisplayName("更新処理")
 	class Update {
 
 		@Test
-		void 書籍在庫情報リスト更新成功() throws Exception {
-			// Arrange
+		@DisplayName("書籍在庫情報更新成功")
+		void updateSuccess() throws Exception {
 			final String EXPECTED_MSG = "正常に更新されました。";
 			when(bookManagementService.update(5, "test", 100)).thenReturn(0);
 
-			// Act & Assert
 			mockMvc.perform(post("/manage/book")
 							.param("update", "")
 							.param("bookId", "5")
@@ -116,12 +114,11 @@ class BookManagementScreenControllerTest {
 		}
 
 		@Test
-		void 書籍在庫情報リスト取得失敗() throws Exception {
-			// Arrange
+		@DisplayName("書籍在庫情報更新失敗")
+		void updateFailure() throws Exception {
 			final String EXPECTED_MSG = "更新に失敗しました。";
 			when(bookManagementService.update(5, "test", 100)).thenReturn(1);
 
-			// Act & Assert
 			mockMvc.perform(post("/manage/book")
 							.param("update", "")
 							.param("bookId", "5")
@@ -136,15 +133,15 @@ class BookManagementScreenControllerTest {
 	}
 
 	@Nested
+	@DisplayName("発注処理")
 	class Order {
 
 		@Test
-		void 書籍在庫情報リスト更新成功() throws Exception {
-			// Arrange
+		@DisplayName("書籍発注成功")
+		void orderSuccess() throws Exception {
 			final String EXPECTED_MSG = "正常に発注されました。";
 			when(bookManagementService.order(5, "test", 100)).thenReturn(0);
 
-			// Act & Assert
 			mockMvc.perform(post("/manage/book")
 							.param("order", "")
 							.param("bookId", "5")
@@ -158,12 +155,11 @@ class BookManagementScreenControllerTest {
 		}
 
 		@Test
-		void 書籍在庫情報リスト取得失敗() throws Exception {
-			// Arrange
+		@DisplayName("書籍発注失敗")
+		void orderFailure() throws Exception {
 			final String EXPECTED_MSG = "発注に失敗しました。";
 			when(bookManagementService.order(5, "test", 100)).thenReturn(1);
 
-			// Act & Assert
 			mockMvc.perform(post("/manage/book")
 							.param("order", "")
 							.param("bookId", "5")
